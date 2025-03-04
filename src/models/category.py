4c3a139c0ec512d5f135f2d1c8c601ae5824c2d8
Anton <Antonhasagi@yandex.ru>
2025-03-04 15:26:06 +0300

    final1

--- src/models/category.py
+++ src/models/category.py
@@ -2,13 +2,26 @@ from src.models.product import Product
 
 
 class Category:
-    category_count = 0
-    product_count = 0
+    product_count = 0  # Статическая переменная для отслеживания количества продуктов
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str, products: list = None):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products is not None else []
+        for product in self.__products:
+            if isinstance(product, Product):
+                Category.product_count += 1  # Увеличиваем счетчик при добавлении продукта
 
-        Category.category_count += 1
-        Category.product_count += len(products)
+    @property
+    def products(self):
+        return ", ".join([product.name for product in self.__products])
+
+    def add_product(self, product: Product):
+        if isinstance(product, Product):  # Проверка типа
+            self.__products.append(product)
+            Category.product_count += 1  # Увеличиваем количество товаров в категории
+        else:
+            raise TypeError("Только объект типа Product может быть добавлен.")
+
+    def __repr__(self):
+        return f"Category(name={self.name}, description={self.description}, products={self.products})"
--- src/models/product.py
+++ src/models/product.py
@@ -2,5 +2,27 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price  # Приватный атрибут
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value: float):
+        if value <= 0:
+            raise ValueError("Цена должна быть положительным числом")
+        self.__price = value
+
+    def __repr__(self):
+        return f"Product(name={self.name}, description={self.description}, price={self.price}, quantity={self.quantity})"
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"]
+        )
